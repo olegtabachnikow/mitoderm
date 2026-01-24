9339808d26406a7a78e6a33d67cd700269a65dd2
Oleg Tabachnikow <olegtabachnikow@Olegs-MacBook-Pro.local>
2026-01-24 22:42:41 +0200

    updated seo

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,27 +1,24 @@
 import { MetadataRoute } from 'next';
 
 const baseUrl = 'https://mitoderm.com';
+const langs = ['he', 'en', 'ru'];
+const googleFile = 'googlee699627fee504b66.html';
 
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: [
-          '/api/',
-          '/utils/',
-          '/*/form',
-          '/*/event/form',
-          '/*/event/success',
-        ],
-      },
-      // Allow Google verification file
-      {
-        userAgent: '*',
-        allow: ['/*/googlee699627fee504b66.html'],
-      },
+  const rules = langs.map((lang) => ({
+    userAgent: '*',
+    allow: `/${lang}/${googleFile}`,
+    disallow: [
+      '/api/',
+      '/utils/',
+      '/*/form',
+      '/*/event/form',
+      '/*/event/success',
     ],
+  }));
+  
+  return {
+    rules,
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
