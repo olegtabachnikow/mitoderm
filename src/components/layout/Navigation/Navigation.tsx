472085b8b8183465dc5a4ac8d2562a05c853aad0
Oleg Tabachnikow <olegtabachnikow@Olegs-MacBook-Pro.local>
2026-03-07 00:16:15 +0200

    upd

--- src/components/layout/Navigation/Navigation.tsx
+++ src/components/layout/Navigation/Navigation.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { FC } from 'react';
 import styles from './Navigation.module.scss';
+import { motion } from 'framer-motion';
 import {
   navMainList,
   navEventList,
@@ -12,6 +13,25 @@ import { useTranslations } from 'next-intl';
 import { useMediaQuery } from 'react-responsive';
 import { usePathname, Link } from '@/i18n/routing';
 import NavigationProductButton from './NavigationProductButton/NavigationProductButton';
+
+const navigationVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 20 },
+  show: (i: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.1, delay: i * 0.1 + 0.4 },
+  }),
+};
 
 interface Props {
   isOpen: boolean;
@@ -25,13 +45,24 @@ const Navigation: FC<Props> = ({ isOpen, setIsOpen }) => {
   const isSuccessPage = pathName.includes('success');
   const isDoctorPage = pathName.includes('doctors');
 
-  const navList = (() => {
-    if (isFormPage || isSuccessPage) return navFormList;
-    if (isDoctorPage) return navDoctorList;
-    if (pathName.includes('event')) return navEventList;
+  let navList;
 
-    return navMainList;
-  })();
+  switch (true) {
+    case isFormPage || isSuccessPage:
+      navList = navFormList;
+      break;
+
+    case isDoctorPage:
+      navList = navDoctorList;
+      break;
+
+    case pathName.includes('event'):
+      navList = navEventList;
+      break;
+
+    default:
+      navList = navMainList;
+  }
 
   const randomString = () => (Math.random() + 1).toString(36).substring(7);
 
@@ -44,44 +75,61 @@ const Navigation: FC<Props> = ({ isOpen, setIsOpen }) => {
   return (
     <>
       {isTabletOrMobile ? (
-        <nav
+        <motion.nav
+          variants={navigationVariants}
+          initial="hidden"
+          animate={isOpen ? 'show' : 'hidden'}
           aria-label="Main Navigation"
           className={`${styles.mobileNavigation} ${isOpen && styles.active}`}
         >
           <NavigationProductButton isMobile handleClick={handleClick} />
           {navList.map((item: NavItem, index: number) => (
-            <div
+            <motion.div
+              custom={index}
+              variants={itemVariants}
               className={styles.linkContainerMobile}
               key={`item${index + Math.random()}`}
             >
               {!item.scrollId && !item.url ? (
-                <button
+                <motion.button
+                  custom={index}
+                  variants={itemVariants}
                   onClick={handleClick}
                   key={index + randomString()}
                   className={styles.buttonMobile}
                 >
                   {t(item.text)}
-                </button>
+                </motion.button>
               ) : (
-                <Link
-                  onClick={handleClick}
-                  href={item.url ? item.url : `#${item.scrollId}`}
-                  key={index + randomString()}
-                  className={styles.buttonMobile}
-                >
-                  {t(item.text)}
-                </Link>
+                <motion.div variants={itemVariants} custom={index}>
+                  <Link
+                    onClick={handleClick}
+                    href={item.url ? item.url : `#${item.scrollId}`}
+                    key={index + randomString()}
+                    className={styles.buttonMobile}
+                  >
+                    {t(item.text)}
+                  </Link>
+                </motion.div>
               )}
-            </div>
+            </motion.div>
           ))}
-          <Link
-            onClick={handleClick}
-            href={'/'}
-            className={styles.buttonMobile}
-          >
-            {t('navigation.homepage')}
-          </Link>
-        </nav>
+          {pathName.length > 1 && (
+            <motion.div
+              custom={navList?.length}
+              variants={itemVariants}
+              className={styles.linkContainerMobile}
+            >
+              <Link
+                onClick={handleClick}
+                href={'/'}
+                className={styles.buttonMobile}
+              >
+                {t('navigation.homepage')}
+              </Link>
+            </motion.div>
+          )}
+        </motion.nav>
       ) : (
         <nav
           className={`${styles.navigation} ${
--- src/components/layout/Navigation/NavigationProductButton/NavigationProductButton.tsx
+++ src/components/layout/Navigation/NavigationProductButton/NavigationProductButton.tsx
@@ -3,6 +3,29 @@ import styles from './NavigationProductButton.module.scss';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useLocale, useTranslations } from 'next-intl';
+import { motion } from 'framer-motion';
+
+const variants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.1 },
+  },
+};
 
 interface Props {
   isMobile?: boolean;
@@ -18,53 +41,65 @@ const NavigationProductButton: FC<Props> = ({ isMobile, handleClick }) => {
   }, [isMobile]);
 
   return (
-    <div
+    <motion.div
+      variants={variants}
       onMouseEnter={() => (isMobile ? null : setIsOpen(true))}
       onMouseLeave={() => (isMobile ? null : setIsOpen(false))}
       className={`${isMobile ? styles.buttonMobile : styles.button} ${
         styles.dropDownButton
       } ${!isOpen ? styles.closed : styles.opened}`}
     >
-      <Link href={`/${locale}/`}>{t('navigation.product')}</Link>
-      <Image
-        className={`${styles.arrowIcon} ${
-          locale === 'he' ? styles.reversed : ''
-        } ${isOpen ? styles.opened : ''}`}
-        src="/images/icons/arrowDown.svg"
-        width={10}
-        height={5.5}
-        alt="arrow icon"
-      />
+      <motion.div variants={itemVariants}>
+        <Link onClick={handleClick} href={`/${locale}/`}>
+          {t('navigation.product')}
+        </Link>
+        <Image
+          className={`${styles.arrowIcon} ${
+            locale === 'he' ? styles.reversed : ''
+          } ${isOpen ? styles.opened : ''}`}
+          src="/images/icons/arrowDown.svg"
+          width={10}
+          height={5.5}
+          alt="arrow icon"
+        />
+      </motion.div>
       {isOpen && (
-        <div
+        <motion.div
+          variants={variants}
           className={`${styles.dropDownList} ${
             isMobile && styles.dropdownListMobile
           } ${locale === 'he' && styles.reversed}`}
         >
-          <Link
-            onClick={handleClick}
-            className={`${styles.link} ${isMobile && styles.linkMobile}`}
-            href={`../${locale}/exotechgel`}
-          >
-            Exotech Gel
-          </Link>
-          <Link
-            onClick={handleClick}
-            className={`${styles.link} ${isMobile && styles.linkMobile}`}
-            href={`../${locale}/exosignalhairspray`}
-          >
-            Exosignal Hair Spray
-          </Link>
-          <Link
-            onClick={handleClick}
-            className={`${styles.link} ${isMobile && styles.linkMobile}`}
-            href={`../${locale}/exosignal_hair`}
-          >
-            Exosignal Hair
-          </Link>
-        </div>
+          <motion.div variants={itemVariants}>
+            <Link
+              onClick={handleClick}
+              className={`${styles.link} ${isMobile && styles.linkMobile}`}
+              href={`../${locale}/exotechgel`}
+            >
+              Exotech Gel
+            </Link>
+          </motion.div>
+          <motion.div variants={itemVariants}>
+            <Link
+              onClick={handleClick}
+              className={`${styles.link} ${isMobile && styles.linkMobile}`}
+              href={`../${locale}/exosignalhairspray`}
+            >
+              Exosignal Hair Spray
+            </Link>
+          </motion.div>
+          <motion.div variants={itemVariants}>
+            <Link
+              onClick={handleClick}
+              className={`${styles.link} ${isMobile && styles.linkMobile}`}
+              href={`../${locale}/exosignal_hair`}
+            >
+              Exosignal Hair
+            </Link>
+          </motion.div>
+        </motion.div>
       )}
-    </div>
+    </motion.div>
   );
 };
 
