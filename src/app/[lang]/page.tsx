44faddf0341af53a5f1cd393047110c33ee38339
Oleg Tabachnikow <olegtabachnikow@Olegs-MacBook-Pro.local>
2026-04-28 16:47:59 +0300

    uncomment initial page content

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -155,16 +155,16 @@ export default async function HomePage({
   return (
     <>
       <main id="mainpage">
-        {/* <Intro />
+        <Intro />
         <Chevron page="intro" imageName="chevronImage" />
         <Synergy />
         <HowCanBeUsed page="main" />
         <Solution page="main" />
         <Gallery />
-        <Mission /> */}
+        <Mission />
         <DoctorList doctors={doctors} />
-        {/* <Faq />
-        <Contact /> */}
+        <Faq />
+        <Contact />
       </main>
       <Script
         id="faq-schema"
